e9d845ef5ce9c673cdcf94e18c9adfe1fb6839ac
Pedro Meruge <a100709@alunos.uminho.pt>
2024-07-03 23:37:22 +0100

    Started empty squares CNN

--- pieces_recognition/includes.py
+++ pieces_recognition/includes.py
@@ -1,4 +1,13 @@
 import cv2
+import random
 import numpy as np
 import sys
 from pathlib import Path
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.optimizers import Adam
+
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
--- pieces_recognition/pieces_recognition.py
+++ pieces_recognition/pieces_recognition.py
@@ -6,7 +6,8 @@ import board_recognition.parameters as BoardParams
 def process_empty_spaces(board_img, corner_points):
     squares_imgs = split_squares(board_img, corner_points)
 
-    show_result(squares_imgs, writeToFile=False)
+    # show_result(squares_imgs, writeToFile=False)
+    build_CNN()
     
     return squares_imgs
 
@@ -26,6 +27,7 @@ def split_squares(board_img, corner_points):
             tiles = board_img[start_y : end_y, start_x : end_x] # cols, rows
             squares.append(tiles)
 
+    squares = np.array(squares)
     return squares
 
 def show_result(chess_squares, writeToFile=False):
@@ -39,9 +41,60 @@ def show_result(chess_squares, writeToFile=False):
             final_path = out_path + "save/" + int(i)
             cv2.imwrite(final_path, square)
 
-    print("Square size (WxH):", chess_squares[0].shape[1], "x", chess_squares[0].shape[0])
-    #popup window with first square as example
-    cv2.imshow('Result Image', chess_squares[7])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    grid_rows = grid_cols = 8
+    
+    _, axes = plt.subplots(grid_rows, grid_cols, figsize=(8,8))
+    axes = axes.flatten()
+    
+    for idx, img in enumerate(chess_squares):
+        if idx >= grid_rows * grid_cols:
+            break
+        
+        #swap images to RGB
+        resized_img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        # display img
+        axes[idx].imshow(resized_img_rgb)
+        axes[idx].axis('off')
+    
+    # Turn off axes for any remaining empty subplots
+    for i in range(idx + 1, grid_rows * grid_cols):
+        axes[i].axis('off')
+    
+    plt.tight_layout()
+    plt.show()
 
+    print("Square size (WxH):", chess_squares[0].shape[1], "x", chess_squares[0].shape[0])
+
+def build_CNN(input_shape=(100,100,3)): #
+    model = Sequential()
+
+    #1 conv Block
+    model.add(Conv2D(filters=16, kernel_size=(5,5), strides=(1,1), activation='relu', input_shape=input_shape)) # input_shape é imagem de 100x100 com 3 canais RGB
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2))) # a cada 2x2 píxeis simplifica -> stride também é 2
+
+    #2 conv Block
+    model.add(Conv2D(filters=32, kernel_size=(5,5), strides=(1,1), activation='relu')) # input_shape é imagem de 100x100 com 3 canais RGB
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2))) # a cada 2x2 píxeis simplifica -> stride também é 2
+
+    #3 conv Block
+    model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu')) # input_shape é imagem de 100x100 com 3 canais RGB
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2))) # a cada 2x2 píxeis simplifica -> stride também é 2
+
+    #reshape para passar a uma vetor de 640,000 dimensões (correto??)
+    model.add(Flatten())
+
+    model.add(Dense(1000, activation='relu'))
+
+    model.add(Dense(256, activation='relu'))
+
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(
+        optimizer=Adam(learning_rate=1e-3), 
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+    
+    model.summary()
+
+    # model.fit(x_train, y_train, batch_size=128, epochs=3, validation_data=(x_val,y_val))
